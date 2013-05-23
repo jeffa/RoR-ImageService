bd190ac3d847adeca9591284f318c1f74714a08f
jeffa <jeffa@unlocalhost.com>
2013-05-22 21:11:07 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRImageservice::Application.config.secret_token = 'b153abc4fc85fcf66194850cd03277da28fafea8b28a45c5586af279473957440eb554ba487bdc24213213db682e77df22de274ee555a796861d486b3bdaefc6'
